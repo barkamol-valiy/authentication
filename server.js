e6bd658695275c5eb5973b55bb3dcce4ed89f993
barkamol-valiy <barkamol.valiy@gmail.com>
2025-01-01 12:36:45 +0500

    route handling

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const main = require("./routes/main/mainroutes");
+const portfinder = require("portfinder");
 
 const app = express();
 dotenv.config();
+
+app.use("/", main);
+
+portfinder.basePort = process.env.PORT || 3001; //- Start searching from this port
+
+portfinder.getPort((err, port) => {
+  if (err) {
+    console.error("Error finding available port:", err);
+    return;
+  }
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+});
